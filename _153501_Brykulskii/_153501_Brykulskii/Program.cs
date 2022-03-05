80cef1d97b11080c835c2fce47a4769c2acfa9b6
SergeyBrykulskii <serxio6758sk@gmail.com>
2022-03-05 14:09:08 +0300

    First lab

---
+++ _153501_Brykulskii/_153501_Brykulskii/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _153501_Brykulskii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, res;
+
+            Console.WriteLine("Введите делимое:  ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите делитель:  ");
+            b = Convert.ToInt32(Console.ReadLine());
+            res = a / b;
+            Console.WriteLine($"Частное от деления: {res}");
+        }
+    }
+}
