eed94d4904443c1b6a36d1abb9592cd80e559b3c
SergeyBrykulskii <serxio6758sk@gmail.com>
2022-03-06 13:10:59 +0300

    2 lab

---
+++ laba2/laba2_1/laba2_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace laba2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            string choice;
+            double a, b, c;
+
+            Console.WriteLine("Эта программа определяет есть ли решение уравнения вида ах^2 + bx + c = 0 в вещественных числах\n");
+            while(flag)
+            {
+                Console.WriteLine("Хотите продолжить(введите yes, чтобы продолжить, no, чтобы выйти)\n");
+
+                choice = Console.ReadLine(); 
+                switch (choice)
+                {
+                    case "yes":
+                        Console.Write("Введите вещественные a, b, c\na:   ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        
+                        Console.Write("\nb:   ");
+                        b = Convert.ToDouble(Console.ReadLine());
+
+                        Console.Write("\nc:   ");
+                        c = Convert.ToDouble(Console.ReadLine());
+                        
+                        if (b * b - 4 * a * c > 0)
+                        {
+                            Console.Write($"Уравнение {a}x^2 + {b}x + c = 0, имеет два корня\n");
+                        } 
+                        else if (b * b - 4 * a * c == 0)
+                        {
+                            Console.Write($"Уравнение {a}x^2 + {b}x + c = 0, имеет один корня\n");
+                        }
+                        else
+                        {
+                            Console.Write($"Уравнение {a}x^2 + {b}x + c = 0, не имеет вещественных корней\n");
+                        }
+                        break;
+                    case "no":
+                        flag = false;
+                        break;
+                    default:
+                        Console.Write("Введите yes или no\n");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ laba2/laba2_1/laba2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace laba2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.Write("Введите координаты точки (x, y)\n x =   ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n y =   ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+           // x = Math.Abs(x);
+
+            if (Math.Abs(y) < Math.Sqrt(100 - x * x) && y < Math.Abs(x))
+            {
+                Console.WriteLine("Точка находится внутри заданной области");
+            } 
+            else if (Math.Abs(y) <= Math.Sqrt(100 - x * x) && y <= Math.Abs(x))
+            {
+                Console.WriteLine("Точка находится на границе заданной области");
+            }
+            else
+                Console.WriteLine("Точка находится вне заданной области");
+        }
+    }
+}
