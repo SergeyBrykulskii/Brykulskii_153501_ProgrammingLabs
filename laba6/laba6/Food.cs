0981dece771fa0ca2c72f86ad712d47c25f7f002
SergeyBrykulskii <serxio6758sk@gmail.com>
2022-05-12 14:04:11 +0300

    added lab

---
+++ laba6/laba6/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    
+namespace laba6
+{
+    abstract class Food : Products
+    {
+        public Food(string name, int price, int weight) : base(name, price, weight)
+        {
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Название: {0}", Name);
+            Console.WriteLine("Цена: {0}", Price);
+            Console.WriteLine("Вес: {0}", Weight);
+        }
+        
+        
+    }
+}
---
+++ laba6/laba6/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba6
+{
+    abstract class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public abstract double Weight { get; set; }
+        public Products(string name, int price, int weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+        public abstract void Show();
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Продукт: " + this.Name);
+            Console.WriteLine("Цена: " + Price);
+        }
+    }
+}
